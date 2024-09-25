3cd6bba6e2653040d1aa8291be241c057adf6a22
tuan6100 <tuananhho378@gmail.com>
2024-09-25 20:42:24 +0700

    update problem 2.2.5

--- lab01/src/Caculation.java
+++ lab01/src/Caculation.java
@@ -10,20 +10,24 @@ public  class Caculation {
     public double mul(double a, double b) {
         return a * b;
     }
-    public double div(double a, double b) throws ArithmeticException {
+    public double div(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
         return a / b;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double x, y;
-        System.out.println("Enter the first operand: ");
+        System.out.print("Enter the first operand: ");
         x = sc.nextDouble();
-        System.out.println("Enter the second operand: ");
+        System.out.print("Enter the second operand: ");
         y = sc.nextDouble();
         Caculation cal = new Caculation();
-        System.out.println("Sum of two operands is: %,2f", cal.add(x, y));
-        System.out.println("Difference of two operands is: %,2f", cal.sub(x, y));
-        System.out.println("Product of two operands is: %,2f", cal.mul(x, y));
-        System.out.println("Sum of two operands is: %,2f", cal.div(x,y));
+        System.out.printf("Sum of two operands is: %.2f%n", cal.add(x, y));
+        System.out.printf("Difference of two operands is: %.2f%n", cal.sub(x, y));
+        System.out.printf("Product of two operands is: %.2f%n", cal.mul(x, y));
+        System.out.printf("Quotient of two operands is: %.2f%n", cal.div(x, y));
+        sc.close();
     }
 }
