ea45528195279a446b185cddcb3c8a59124257d2
tuan6100 <tuananhho378@gmail.com>
2024-10-30 08:04:48 +0700

    Update lab2

--- lab02/src/hotuananh20226100/Aims_HoTuanAnh20226100.java
+++ lab02/src/hotuananh20226100/Aims_HoTuanAnh20226100.java
@@ -3,22 +3,22 @@ package hotuananh20226100;
 public class Aims_HoTuanAnh20226100 {
     public static void main(String[] args) {
           //Tạo giỏ hàng trống
-        Cart_HoTuanAnh20226100 cart = new Cart_HoTuanAnh20226100();
+        Cart_HoTuanAnh20226100 anOrder = new Cart_HoTuanAnh20226100();
     
         //Thêm đĩa vào giỏ hàng
         DigitalVideoDisc_HoTuanAnh20226100 dvd1 = new DigitalVideoDisc_HoTuanAnh20226100("The Lion King","Animation", "Roger Allers",87,19.95f);
-        cart.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd1);
         DigitalVideoDisc_HoTuanAnh20226100 dvd2 = new DigitalVideoDisc_HoTuanAnh20226100("Star wars","Science Fiction", "Geogre Lucas",87,24.95f);
-        cart.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd2);
         DigitalVideoDisc_HoTuanAnh20226100 dvd3 = new DigitalVideoDisc_HoTuanAnh20226100("Aladin","Animation",18.99f);
-        cart.addDigitalVideoDisc(dvd3);
+        anOrder.addDigitalVideoDisc(dvd3);
     
         //Kiểm tra khi chưa thực hiện hàm xoá            
-        cart.print();
+        anOrder.print();
     
         //Kiểm tra khi dã thực hiện hàm xoá
-        cart.removeDigitalVideoDisc(dvd2);
+        anOrder.removeDigitalVideoDisc(dvd2);
         //Hàm System.out.printf() định dạng in như ngôn ngữ C/C++
-         cart.print();
+         anOrder.print();
     }
 }
