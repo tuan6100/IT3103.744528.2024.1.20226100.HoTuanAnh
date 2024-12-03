338a9faeb6139049bcfddc60ff53dc8ce7ebecac
tuan6100 <tuananhho378@gmail.com>
2024-12-04 00:54:48 +0700

    Upload lab04 to branch release/lab04

---
+++ lab04/AimsProject/src/hust/soict/itep/test/cart/CartTest.java
@@ -0,0 +1,26 @@
+package hust.soict.itep.test.cart;
+
+import hust.soict.itep.aims.cart.Cart;
+import hust.soict.itep.aims.disc.DigitalVideoDisc;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation",
+                "Roger Allers",87,19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star wars","Science Fiction",
+                "Geogre Lucas",87,24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+
+        cart.print();
+
+        cart.searchById(3);
+        cart.searchById(4);
+
+        cart.searchByTitle("The Lion King");
+        cart.searchByTitle("abc");
+    }
+}
